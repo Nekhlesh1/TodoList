3aed5f5faec56bc9e000902c57dd4a6669c23d56
Nekhlesh SIngh <38405081+Nekhlesh1@users.noreply.github.com>
2023-07-30 09:45:25 +0530

    Add files via upload
    Todo list website

---
+++ script.js
@@ -0,0 +1,36 @@
+
+const list = document.getElementById('list');
+const inputBox = document.getElementById('input-box');
+const category = document.getElementById('category');
+function addItem() {
+    if (inputBox.value === '') {
+        alert("Task cannot be null!!");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        list.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7"
+        li.appendChild(span);
+    }
+    inputBox.value = '';
+    saveData();
+}
+list.addEventListener('click', function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+function saveData(){
+    localStorage.setItem("data",list.innerHTML);
+}
+function showData(){
+    list.innerHTML = localStorage.getItem('data');
+}
+showData();
